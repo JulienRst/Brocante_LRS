b64835ff7e0f732842119e95a01d046d0537f5e6
JulienRst <julien.rousset01@gmail.com>
2015-04-12 16:14:12 +0200

    S2 - 3
    I don't know

--- inc/main.inc.php
+++ inc/main.inc.php
@@ -12,18 +12,23 @@ require_once ROOT."\inc\gestionclient.inc.php";
 // stocke l'url demandée par l'utilisateur : utile pour les redirections
 $_SESSION['request_url'] = URLBASE.$_SERVER['REQUEST_URI'];
 
+
 /*Redirection du client s'il n'est pas connecté*/
+
 if(!estConnecte())
 {
-	if(doitSeConnecter())
+	if(doitSeConnecter()){
 		connexionClient();
-	elseif(doitSInscrire())
-		inscriptionClient();
+	}
+	else if(doitSInscrire()){
+		inscriptionClient();l
+	}
 }
 // Securité : l'utilisateur doit être connecté pour afficher la suite.
 connexionCheck();
 
-header('location:'.$_SESSION['request_url']);
+
+//header('location:'.$_SESSION['request_url']);
 
 /*On pense a afficher l'en-tête de la page*/
 require_once ROOT."\inc\main\header.inc.php";
